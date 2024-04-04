bc5a68fe49c2fce80185d681d2e40bf3834ceb1a
Onii-Chan <140295418+waqarbaig6133@users.noreply.github.com>
2024-04-04 01:19:29 -0400

    Update Area and perimeter of ellipse.py

--- Area and perimeter of ellipse.py
+++ Area and perimeter of ellipse.py
@@ -1,3 +1,12 @@
+'''
+Based on the received dimensions, a and b, of an ellipse, calculare its area and perimeter.
+ex)
+Input: ellipse(5,2)
+
+Output: "Area: 31.4, perimeter: 23.1"
+
+'''
+
 from math import pi
 from math import sqrt
 def ellipse(a, b):
